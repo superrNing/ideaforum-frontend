dd6f4174a8741af9238cb40bb229212a839ae3e3
Ning <luning0715@gmail.com>
2024-07-29 21:25:08 -0400

    init register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,22 +5,20 @@ import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import { useState } from "react";
 import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useMutation } from "@tanstack/react-query";
-import { userLogin, UserLoginParams } from "@/app/api/login";
+import { createUser, CreateUserParams } from "@/app/api/login";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { Snackbar } from "@mui/material";
 import { useRouter } from "next/navigation";
 import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
 import toast from "react-hot-toast";
+import { pathOr } from "ramda";
+import SensorOccupiedIcon from "@mui/icons-material/SensorOccupied";
 function Copyright(props: any) {
   return (
     <Typography
@@ -36,7 +34,6 @@ function Copyright(props: any) {
   );
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
 export default function SignInSide() {
@@ -47,10 +44,6 @@ export default function SignInSide() {
   } = useForm();
   const router = useRouter();
   const [open, setOpen] = useState<boolean>(false);
-
-  const handleClick = () => {
-    setOpen(true);
-  };
 
   const handleClose = (
     event?: React.SyntheticEvent | Event,
@@ -63,24 +56,28 @@ export default function SignInSide() {
   };
 
   const mutation = useMutation({
-    mutationFn: userLogin,
+    mutationFn: createUser,
     onSuccess: async (res) => {
       console.log(res, "res");
 
-      if (res?.status === 200) {
-        toast.success("logged in!");
+      if (res?.status === 201) {
+        toast.success("Registered!");
         localStorage.setItem("user_token", res?.data?.token);
         localStorage.setItem("user_id", res?.data?.user_id);
         router.push("/dashboard");
       }
     },
     onError: (error) => {
-      toast.error("Wrong email or password");
-      console.log(error, "error");
+      const msg = pathOr(
+        "Something Wrong.",
+        ["response", "data", "message"],
+        error
+      );
+      toast.error(msg);
     },
   });
 
-  const onSubmit: SubmitHandler<UserLoginParams> = (data) => {
+  const onSubmit: SubmitHandler<CreateUserParams> = (data) => {
     mutation.mutate({
       ...data,
     });
@@ -98,10 +95,10 @@ export default function SignInSide() {
           }}
         >
           <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
+            <SensorOccupiedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            Sign up
           </Typography>
           <Box
             component="form"
@@ -111,12 +108,20 @@ export default function SignInSide() {
           >
             <TextField
               margin="normal"
+              autoFocus
+              required
+              fullWidth
+              id="name"
+              label="User Name"
+              {...register("name", { required: true })}
+            />
+            <TextField
+              margin="normal"
               required
               fullWidth
               id="email"
               label="Email Address"
               autoComplete="email"
-              autoFocus
               {...register("email", { required: true })}
             />
             <TextField
@@ -129,9 +134,15 @@ export default function SignInSide() {
               autoComplete="current-password"
               {...register("password", { required: true })}
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Confirm Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              {...register("password_confirmation", { required: true })}
             />
             <Button
               type="submit"
@@ -139,28 +150,8 @@ export default function SignInSide() {
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign In
+              Sign Up
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link
-                  onClick={() => setOpen(true)}
-                  className="cursor-pointer"
-                  variant="body2"
-                >
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link
-                  href="/register"
-                  className="cursor-pointer"
-                  variant="body2"
-                >
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
             <Copyright sx={{ mt: 5 }} />
           </Box>
         </Box>
