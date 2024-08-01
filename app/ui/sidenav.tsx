ce99c855b32afe99add6a591c5298b6b00f660a8
Zhongrui Liu <liu00642@algonquinlive.com>
2024-08-01 16:35:14 -0400

    logo

--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from 'next/image';
 import NavLinks from "@/app/ui/nav-links";
 import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew";
 import Dialog from "@/app/ui/dialog";
@@ -18,11 +19,14 @@ export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-center justify-center rounded-md bg-blue-600 p-4 md:h-40"
+        className="mb-2 flex h-[180px] items-center justify-center rounded-md bg-cover bg-center p-4 "
         href="/dashboard"
+        style={{
+          backgroundImage: `url('/img/logo1.jpg')`, 
+        }}
       >
-        <div className="w-32 text-white md:w-40 ">
-          <h2>SOME COOL LOGO</h2>
+        <div className="h-[200px] md:w-40">
+          
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
